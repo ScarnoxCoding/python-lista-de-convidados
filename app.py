ce9e9739212edff05ab6d01bb6495ee2bcc776c6
Darlei Severo <144695584+ScarnoxCoding@users.noreply.github.com>
2025-08-25 09:40:24 -0300

    Criei as listas

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 # Lista de Convidados que foram convidados para a festa
+convidados_da_festa = ['Nayelli','Igor','Gabriel','Bianca','Daniela', 'Breno']
+
+# Dicionário vazio para  armazenar o status de presença de cada pessoa
+status_presenca = {}
+
+# Esta é a lista de pessoas que tentaram entrar na festa
+lista_de_chegadas = ['Igor','Gabriel','Bianca','Daniela']
